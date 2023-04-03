b7fee86272bd0b13ed65cc881e15d783842b91c5
pouria <pouria.msd@gmail.com>
2023-04-03 16:42:41 +0200

    shop page basis

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { PRODUCTS } from "../../products";
+import Product from "./Product";
+import "./shop.css";
 
 const Shop = () => {
   return (
-    <div>Shop</div>
-  )
-}
+    <div className="shop">
+      <div className="shopTitle">
+        <h1>Samadi Shop</h1>
+      </div>
+
+      <div className="products">
+        {PRODUCTS.map((product) => (
+          <Product data={product} key={product.id}/>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Shop
